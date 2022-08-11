5655022bf494f37fe9f6cb3ce9e13530e8575cd0
Abdaleh Hersi <therealhersi03@gmail.com>
2022-08-11 18:34:49 +0100

    completed OBmDAPI

--- assets/js/callOmbdAPI.js
+++ assets/js/callOmbdAPI.js
@@ -1,35 +1,35 @@
-function callOmbdAPI(movieTitle, SeasonNumber){ //trial for the Ombd API 
-    console.debug("call 1st API");
-    // get all parameter and combined it into the search link
-    var testingURL ="https://www.omdbapi.com/?t="+"Game%20of%20Thrones"/*movie title*/+"&Season=1&Episode=1&apikey=9abdf83f";
-    console.debug(testingURL);
+// function callOmbdAPI(movieTitle, SeasonNumber){ //trial for the Ombd API 
+//     console.debug("call 1st API");
+//     // get all parameter and combined it into the search link
+//     var testingURL ="https://www.omdbapi.com/?t="+"Game%20of%20Thrones"/*movie title*/+"&Season=1&Episode=1&apikey=9abdf83f";
+//     console.debug(testingURL);
 
-    fetch(testingURL)
-    .then(function (response) {
-      if (!response.ok) {
-        console.debug(response);
-        throw response.json();
-      }
-      return response.json();
-    })
-    .then(function (data) {
-        console.debug(data);
-      if(data.length === 0 ){
-        console.debug("No data");       //No data found so should show modal window 
-      }
-      else{                            
-         //should store the data detail to local storage 
-        //callYoutubeAPI();  //Call next API function 
+//     fetch(testingURL)
+//     .then(function (response) {
+//       if (!response.ok) {
+//         console.debug(response);
+//         throw response.json();
+//       }
+//       return response.json();
+//     })
+//     .then(function (data) {
+//         console.debug(data);
+//       if(data.length === 0 ){
+//         console.debug("No data");       //No data found so should show modal window 
+//       }
+//       else{                            
+//          //should store the data detail to local storage 
+//         //callYoutubeAPI();  //Call next API function 
 
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
+//       }
+//     })
+//     .catch(function (error) {
+//       console.error(error);
 
         
-    });
+//     });
 
     
   
   
-  }
+//   }
