15d1fc622b9fcf85c73bf0371b9643fba53550ed
DMon <davemonaghan@gmx.com>
2022-08-15 16:40:37 +0100

    updated media queary settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,13 +88,3 @@ function searchFunction(){
 
 init();
 
-// var homeBtn = document.getElementById("home-btn")
-
-
-
-// function homepage (){
-//     window.location.href = "./index.html";
-// }
-
-
-// homeBtn.addEventListner('click', homepage)
